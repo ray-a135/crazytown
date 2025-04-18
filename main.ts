27e85e66ab7260677ae29d5b192e8f31585ac7c7
ray-a135 <rachelauguste36@gmail.com>
2025-04-18 16:37:25 -0400

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,43 @@
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
-    tiles.setCurrentTilemap(tilemap`the town`)
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile19`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level14`)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC, function (sprite, otherSprite) {
+    game.splash("There is something going on!")
+    game.showLongText("What?", DialogLayout.Bottom)
+    game.splash("There is a party at any house!")
+    pause(2000)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level12`)
+    mySprite.setPosition(78, 52)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile29`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level13`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`waterdoor`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level14`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level12`)
+    mySprite.setPosition(21, 13)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile8`, function (sprite, location) {
     tiles.setCurrentTilemap(tilemap`store`)
 })
-let mySprite = sprites.create(img`
+scene.onOverlapTile(SpriteKind.Player, assets.tile`firedoor`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`the town`)
+    mySprite.setPosition(78, 52)
+    controller.moveSprite(mySprite)
+    scene.cameraFollowSprite(mySprite)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile27`, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level13`)
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     f 2 2 2 2 2 2 f f f f 2 2 2 2 2 
     f 2 2 2 2 2 2 f f f f 2 2 2 2 2 
     f 2 2 2 2 2 2 f f f f 2 2 2 2 2 
@@ -25,3 +58,25 @@ let mySprite = sprites.create(img`
 controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`the town`)
+mySprite.setPosition(19, 6)
+let mySprite2 = sprites.create(img`
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 f f f f f f f f 2 f f f f 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    `, SpriteKind.NPC)
+game.onUpdateInterval(500, function () {
+    mySprite2.setVelocity(randint(0, 10), randint(0, 10))
+})
